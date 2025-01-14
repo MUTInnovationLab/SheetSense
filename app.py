b6a35d46dd8498cfd7557ec3b5ea0edc34e79baa
SiyabongaKhayile <siyabongabongumusa895@gmail.com>
2025-01-14 11:54:35 +0200

    hhh

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, url_for
+# make_response
+import os
 
 app = Flask(__name__)
 
@@ -17,6 +19,20 @@ def view_history():
 @app.route('/manage_files')
 def manage_files():
     return render_template('files.html')
+
+# @app.route('/static/<path:filename>') 
+# def custom_static(filename):
+#     response = make_response(send_from_directory(os.path.join(app.root_path, 'static'), filename)) 
+#     response.headers['Cache-Control'] = 'public, max-age=31536000, immutable' 
+#     return response
+
+@app.route('/static/<path:filename>') 
+def custom_static(filename): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
+
+@app.route('/favicon.ico') 
+def favicon(): return send_from_directory(os.path.join(app.root_path, 'static'), 
+                                          'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run(debug=True)
