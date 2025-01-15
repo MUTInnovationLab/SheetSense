fdd35888831d5c0427dc7b0d35f020024c5f9f73
nkosi <nkosiphila901@gmail.com>
2025-01-15 08:57:28 +0200

    firebase (CVSYSTEM) config

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-y5cGo8Da9eFCICNSw8V9O9bZxKGLtg4",
-  authDomain: "sheetsensedb.firebaseapp.com",
-  projectId: "sheetsensedb",
-  storageBucket: "sheetsensedb.firebasestorage.app",
-  messagingSenderId: "834503780189",
-  appId: "1:834503780189:web:4db6a0c7f0cf486ce02ab4",
-  measurementId: "G-6GW7RKRQL4"
+  apiKey: "AIzaSyBvLwOd23fTx6RpAuodsyFZwntry6AV9Ik",
+  authDomain: "cvsystem-253f9.firebaseapp.com",
+  projectId: "cvsystem-253f9",
+  storageBucket: "cvsystem-253f9.appspot.com",
+  messagingSenderId: "133527156661",
+  appId: "1:133527156661:web:8d8794eeae33b16dabcbfc"
 };
-//
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
